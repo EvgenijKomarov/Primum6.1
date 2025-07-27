d62088ec310ef8f226d6ce7dfcd24b7f95d06d0f
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2025-07-27 10:13:36 +0300

    проба работы API

---
+++ DTO/StudentDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class StudentDTO
+    {
+        public int Id { get; set; }
+
+        public string Password { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public bool IsApproved { get; set; }
+    }
+}
---
+++ PrimumCore/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+using PrimumCore.Models;
+using PrimumPlatformModel.Models.Enums;
+using System.Net.Http;
+
+namespace PrimumCore.Controllers
+{
+    [ApiController]
+    [Route("api/student/[controller]")]
+    public class StudentController(DbContextFactory<IPrimumContext> _contextFactory) : PrimumController
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudent(int id)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            var user = context.Set<User>().FirstOrDefault(x => x.Id == id);
+            if (user is null) { return NotFound(); }
+            if (user.StudentProfile is null) { return NotFound(); }
+
+            return Ok(new StudentDTO
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Patronymic = user.Patronymic,
+                Password = user.Password,
+                IsApproved = user.StudentProfile.ApproveStatus == ApproveStatus.Approved
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegStudent(StudentDTO dto)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var user = new User
+            {
+                Name = dto.Name,
+                Surname = dto.Surname,
+                Patronymic = dto.Patronymic,
+                Password = dto.Password,
+                StudentProfile = new StudentProfile()
+            };
+
+            context.Set<User>().Add(user);
+            await _contextFactory.SafeSaveChangesAsync(context);
+
+            return Ok(user.Id);
+        }
+    }
+}
