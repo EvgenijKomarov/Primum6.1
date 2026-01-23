ddec28c448924070b514c391592614b677e25942
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2026-01-23 09:51:08 +0300

    Added swagger support

--- PrimumCore/Program.cs
+++ PrimumCore/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.AddDI();
 builder.AddPrimumContext();
 builder.AddProjectControllers();
@@ -22,6 +25,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
