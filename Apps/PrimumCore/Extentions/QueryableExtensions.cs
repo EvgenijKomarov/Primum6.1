7b5821164c0f4999f525d6b59a95dbe67e0624ef
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2026-03-13 13:17:16 +0300

    useful extension

---
+++ Apps/PrimumCore/Extentions/QueryableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimumCore.Exceptions;
+using System.Linq.Expressions;
+
+namespace PrimumCore.Extentions
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<TEntity> One<TEntity>(
+            this IQueryable<TEntity> queryable,
+            Expression<Func<TEntity, bool>>? predicate = null,
+            CancellationToken cancellationToken = default)
+            where TEntity : class
+        {
+            var entity = predicate == null
+                ? await queryable.FirstOrDefaultAsync(cancellationToken).ConfigureAwait(false)
+                : await queryable.FirstOrDefaultAsync(predicate, cancellationToken).ConfigureAwait(false);
+
+            return entity ?? throw new NotFoundException(typeof(TEntity).Name);
+        }
+
+        public static async Task<IEnumerable<TEntity>> Many<TEntity>(
+            this IQueryable<TEntity> queryable,
+            CancellationToken cancellationToken = default)
+            where TEntity : class
+        {
+            var entities = await queryable.ToArrayAsync();
+            return entities;
+        }
+    }
+}
