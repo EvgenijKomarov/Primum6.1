db851eb394c091fa2ba529c3b17625c94dad61cd
lisophoria <work.lisophoria@gmail.com>
2026-05-09 19:32:54 +0300

    web: chore user api config

--- Apps/PrimumWeb/src/entity/user/config/user-api.config.ts
+++
@@ -1,8 +0,0 @@
-export const userApiConfig = {
-  getUserInfo: "/user/",
-  sendEmailVerification: "/user/send-email-verification",
-  confirmEmail: "/user/confirm-email-verification",
-  confirmChat: "/user/confirm-chat",
-  createTeacherProfile: "/user/create-teacher-profile",
-  createStudentProfile: "/user/create-student-profile",
-}
