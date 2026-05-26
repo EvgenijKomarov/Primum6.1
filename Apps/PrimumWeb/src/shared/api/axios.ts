a1af03f2404808e60d0ed311967a032690674d4a
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2026-05-26 08:52:36 +0300

    routing improvement

--- Apps/PrimumWeb/src/shared/api/axios.ts
+++ Apps/PrimumWeb/src/shared/api/axios.ts
@@ -1,15 +1,10 @@
 import axios from 'axios';
 import { useUserStore } from '@/entity/user';
 
-const {
-  VITE_PUBLIC_WEB_API,
-} = import.meta.env;
-
-export const fetcherInstance = axios.create({
-  baseURL: VITE_PUBLIC_WEB_API,
-});
+export const fetcherInstance = axios.create();
 
 fetcherInstance.interceptors.request.use((config) => {
+  config.baseURL = '/api';
   const token = useUserStore.getState().token;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
--- Apps/PrimumWeb/vite.config.ts
+++ Apps/PrimumWeb/vite.config.ts
@@ -2,19 +2,36 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from "path";
 import tailwindcss from '@tailwindcss/vite';
+import { env } from 'process';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  css: {
-    devSourcemap: false,
-  },
-  optimizeDeps: {
-    force: true,
-  }
-})
+    css: {
+      devSourcemap: false,
+    },
+    server: {
+      host: '127.0.0.1',
+      port: 5173,
+      proxy: {
+        '/config': {
+          target: env.VITE_CONFIG_SERVICE_URL || 'http://localhost:5000',
+          changeOrigin: true,
+        },
+        '/api': {
+          target: env.VITE_WEB_API_URL ||'http://localhost:5002',
+          changeOrigin: true,
+        },
+      },
+    },
+    optimizeDeps: {
+      force: true,
+    },
+  };
+});
