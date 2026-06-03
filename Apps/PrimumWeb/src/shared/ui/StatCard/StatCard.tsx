967f22e5b406ebde6242c33066cd53046b54f0c0
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2026-06-03 15:20:50 +0300

    info popups rework

--- Apps/PrimumWeb/src/shared/ui/StatCard/StatCard.tsx
+++ Apps/PrimumWeb/src/shared/ui/StatCard/StatCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const StatCard = ({ title, value }: Props) => {
     return (
     <div className={styles.stat}>
-        {title && <h3 className={styles.statLabel}>{title}</h3>}
-        {value && <p className={styles.statValue}>{value}</p>}
+        {title && <h6 className={styles.statLabel}>{title}</h6>}
+        {value && <span className={styles.statValue}>{value}</span>}
     </div>)
 };
