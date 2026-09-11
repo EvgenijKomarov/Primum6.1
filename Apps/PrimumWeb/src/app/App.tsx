efbf2092095ea14d116f8589dede0e1a40bb457f
EvgeniyKomarov <evgeniy.komarov@icl-services.com>
2026-09-11 17:36:31 +0300

    created container for content

--- Apps/PrimumWeb/src/app/App.tsx
+++ Apps/PrimumWeb/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { AppRouter } from '@/app/providers/AppRouter.tsx';
 import { CurrentUserProvider } from '@/app/providers/CurrentUserProvider';
+import { PageContainer } from '@/shared/ui/PageContainer/PageContainer';
 import { ToastProvider } from '@/shared/ui/Toast/ToastContext';
 import { Header } from '@/widgets/header';
 import { ModalRoot } from '@/widgets/modal';
@@ -9,7 +10,9 @@ function App() {
     <CurrentUserProvider>
       <Header />
       <ToastProvider>
-        <AppRouter />
+        <PageContainer>
+          <AppRouter />
+        </PageContainer>
         <ModalRoot />
       </ToastProvider>
     </CurrentUserProvider>
